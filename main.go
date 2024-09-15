6caeb35feeb8455e3313a3f6ead10d0107e09289
kevwasonga <kevinwasonga116@gmail.com>
2024-09-16 00:15:22 +0300

    Feat: dynamically pick a banner file to display from and colour

--- main.go
+++ main.go
@@ -6,19 +6,41 @@ import (
 	"strings"
 
 	"ascii/asciiArt"
-	// Assuming utils package is accessible here
 )
 
 func main() {
-	fileName := "standard.txt"
+	fileName, err := asciiArt.GetBannerFile()
+	if err != nil {
+		fmt.Println(err)
+		PrintUsage()
+		return
+	}
 
 	// validate accepted number of os arguments
-	if len(os.Args[1:]) < 1 || len(os.Args[1:]) > 3 || (!strings.HasPrefix(os.Args[1], "--color=") && len(os.Args[1:]) != 1) || (strings.HasPrefix(os.Args[1], "--color=") && len(os.Args[1:]) == 1) {
+	if len(os.Args[1:]) < 1 || len(os.Args[1:]) > 4 || (!strings.HasPrefix(os.Args[1], "--color=") && len(os.Args[1:]) != 1) || (strings.HasPrefix(os.Args[1], "--color=") && len(os.Args[1:]) == 1) {
 		fmt.Printf("Usage: go run . [OPTION] [STRING]\n\n")
 		fmt.Println("EX: go run . --color=<color> <letters to be colored> \"something\"")
 		return
 	}
+	validBannerFiles := map[string]bool{
+		"standard":   true,
+		"shadow":     true,
+		"thinkertoy": true,
+	}
 
+	var lastArg string
+
+	// Check if there are any arguments provided at the 4th index, else allocate it to standard
+	if len(os.Args) == 4 {
+		lastArg = "standard" // Default to "standard"
+	} else {
+		lastArg = os.Args[len(os.Args)-1]
+	}
+
+	if !validBannerFiles[lastArg] {
+		PrintUsage()
+		return
+	}
 	// print a new line and exit in case argument is a new line character only
 	if os.Args[1] == "\\n" {
 		fmt.Println()
@@ -59,4 +81,9 @@ func main() {
 	for _, line := range lines {
 		asciiArt.PrintLineBanner(line, substring, color, bannerMap)
 	}
+}
+
+func PrintUsage() {
+	fmt.Printf("Usage: go run . [OPTION] [STRING]\n\n")
+	fmt.Println("EX: go run . --color=<color> <letters to be colored> \"something\"")
 }
